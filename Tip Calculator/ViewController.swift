4f2069c3d8a7a9897cbee5e5bbbd40694a7f74f3
Xiaomin Hu <hxm2102506@gmail.com>
2014-09-05 16:04:04 -0700

    Add Basic UI

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var btnClear: UIButton!
+    @IBOutlet var btnDel: UIButton!
+    @IBOutlet var btnDot: UIButton!
+    @IBOutlet var btn0: UIButton!
+    @IBOutlet var btn1: UIButton!
+    @IBOutlet var btn2: UIButton!
+    @IBOutlet var btn3: UIButton!
+    @IBOutlet var btn4: UIButton!
+    @IBOutlet var btn5: UIButton!
+    @IBOutlet var btn6: UIButton!
+    @IBOutlet var btn7: UIButton!
+    @IBOutlet var btn8: UIButton!
+    @IBOutlet var btn9: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btn0.layer.borderWidth = 0.3
+        btn1.layer.borderWidth = 0.3
+        btn2.layer.borderWidth = 0.3
+        btn3.layer.borderWidth = 0.3
+        btn4.layer.borderWidth = 0.3
+        btn5.layer.borderWidth = 0.3
+        btn6.layer.borderWidth = 0.3
+        btn7.layer.borderWidth = 0.3
+        btn8.layer.borderWidth = 0.3
+        btn9.layer.borderWidth = 0.3
+        btnClear.layer.borderWidth = 0.3
+        btnDel.layer.borderWidth = 0.3
+        btnDot.layer.borderWidth = 0.3
+        self.navigationController.navigationBar.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
